6bb0ea423931c117adc1561f2e5d57ef49d35a78
Luca Gentile <gentilemeister@gmail.com>
2017-11-12 20:29:51 +0100

    Partial removal of electron-vue frontend boilerplate

---
+++ src/renderer/components/HomeScreen.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="wrapper">
+    <main>
+      Test
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home-screen',
+    components: {},
+    methods: {
+    }
+  }
+</script>
+
+<style>
+</style>
