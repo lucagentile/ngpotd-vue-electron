2b15f8f401604810494eb279c1d92492807d17e1
Luca Gentile <gentilemeister@gmail.com>
2017-12-10 17:30:06 +0100

    ApplicationMenu from renderer

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, BrowserWindow, ipcMain } from 'electron'
+import { app, BrowserWindow, ipcMain, Menu } from 'electron'
 import axios from 'axios'
 import fs from 'fs'
 import path from 'path'
@@ -22,6 +22,74 @@ let picturesPath = app.getPath('pictures') + path.sep + 'NGPOTD'
 const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
+
+const template = [
+  {
+    role: 'reload'
+  },
+  {
+    label: 'Donate to National Geographic Society',
+    click () {
+      require('electron').shell.openExternal('https://www.nationalgeographic.org/give/')
+    }
+  },
+  {
+    role: 'quit'
+  }
+]
+
+if (process.env.NODE_ENV === 'development') {
+  template.unshift({
+    label: 'View',
+    submenu: [
+      {role: 'forcereload'},
+      {role: 'toggledevtools'},
+      {type: 'separator'},
+      {role: 'togglefullscreen'}
+    ]
+  })
+}
+
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      {role: 'about'},
+      {type: 'separator'},
+      {role: 'services', submenu: []},
+      {type: 'separator'},
+      {role: 'hide'},
+      {role: 'hideothers'},
+      {role: 'unhide'},
+      {type: 'separator'},
+      {role: 'quit'}
+    ]
+  })
+
+  // Edit menu
+  template[1].submenu.push(
+    {type: 'separator'},
+    {
+      label: 'Speech',
+      submenu: [
+        {role: 'startspeaking'},
+        {role: 'stopspeaking'}
+      ]
+    }
+  )
+
+  // Window menu
+  template[3].submenu = [
+    {role: 'close'},
+    {role: 'minimize'},
+    {role: 'zoom'},
+    {type: 'separator'},
+    {role: 'front'}
+  ]
+}
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 
 function createWindow () {
   /**
