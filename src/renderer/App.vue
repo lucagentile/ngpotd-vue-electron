aadd629578f41466befee38e1a81ed9399cdd881
Luca Gentile <gentilemeister@gmail.com>
2018-05-05 22:53:32 +0200

    Images dealt in ImagesView component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-  import { mapGetters, mapActions } from 'vuex'
+  import { mapGetters } from 'vuex'
   import * as types from './store/types.js'
   import WallpaperModal from './components/WallpaperModal'
   import Titlebar from './components/Titlebar'
@@ -18,34 +18,11 @@
     name: 'ngpotd-electron-vue',
     computed: {
       ...mapGetters({
-        isDownloading: types.GET_IS_DOWNLOADING,
-        pageSize: types.GET_PAGE_SIZE,
-        page: types.GET_IMAGES_CURRENTPAGE
+        isDownloading: types.GET_IS_DOWNLOADING
       }),
       isNotDarwin () {
         return this.$electron.remote.getGlobal('process').env !== 'darwin'
       }
-    },
-    methods: {
-      ...mapActions({
-        pushImage: types.PUSH_IMAGE,
-        setImageCount: types.SET_IMAGES_COUNT
-      })
-    },
-    beforeCreate () {
-      this.$electron.ipcRenderer.on('images:count', (event, count) => {
-        this.setImageCount(count)
-      })
-      this.$electron.ipcRenderer.on('images:push', (event, images) => {
-        this.pushImage(images)
-      })
-    },
-    created () {
-      const pagination = {
-        pageSize: this.pageSize,
-        page: this.page
-      }
-      this.$electron.ipcRenderer.send('image:index', pagination)
     },
     components: {
       WallpaperModal, Titlebar
--- src/renderer/components/ImagesView.vue
+++ src/renderer/components/ImagesView.vue
@@ -29,14 +29,17 @@
       ...mapGetters({
         pageSize: types.GET_PAGE_SIZE,
         imagesCount: types.GET_IMAGES_COUNT,
-        images: types.GET_IMAGES
+        images: types.GET_IMAGES,
+        page: types.GET_IMAGES_CURRENTPAGE
       })
     },
     methods: {
       ...mapActions({
         setImageToSet: types.SET_IMAGE_TO_SET,
         setImageFromName: types.SET_IMAGE_FROM_NAME,
-        setPage: types.SET_PAGE
+        setPage: types.SET_PAGE,
+        setImageCount: types.SET_IMAGES_COUNT,
+        pushImage: types.PUSH_IMAGE
       }),
       askToSetWallpaper (event) {
         this.setImageFromName(event.target.getAttribute('data-name'))
@@ -52,6 +55,19 @@
     },
     components: {
       DownloaderView
+    },
+    created () {
+      this.$electron.ipcRenderer.on('images:count', (event, count) => {
+        this.setImageCount(count)
+      })
+      this.$electron.ipcRenderer.on('images:push', (event, images) => {
+        this.pushImage(images)
+      })
+      const pagination = {
+        pageSize: this.pageSize,
+        page: this.page
+      }
+      this.$electron.ipcRenderer.send('image:index', pagination)
     }
   }
 </script>
