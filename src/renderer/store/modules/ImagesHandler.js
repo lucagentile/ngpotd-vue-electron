4f28e835524afc0c6f9b1d8677e815005137bc29
Luca Gentile <gentilemeister@gmail.com>
2018-05-05 18:45:00 +0200

    Pagination images

--- src/renderer/store/modules/ImagesHandler.js
+++ src/renderer/store/modules/ImagesHandler.js
@@ -3,12 +3,25 @@ import { createImage } from '../../helpers'
 
 const state = {
   images: [],
-  imageToSet: {}
+  imageToSet: {},
+  page: 0,
+  pageSize: 18
 }
 
 const getters = {
+  [types.GET_PAGE_SIZE]: (state) => {
+    return state.pageSize
+  },
   [types.GET_IMAGES]: (state) => {
     return state.images
+  },
+  [types.GET_IMAGES_COUNT]: (state) => {
+    return state.images.length
+  },
+  [types.GET_IMAGES_PAGE]: (state) => {
+    const start = state.pageSize * state.page
+    const end = (state.page + 1) * state.pageSize
+    return state.images.slice(start, end)
   },
   [types.GET_IMAGE_TO_SET]: (state) => {
     return state.imageToSet
@@ -21,6 +34,9 @@ const mutations = {
   },
   [types.SET_IMAGE_TO_SET] (state, image) {
     state.imageToSet = image
+  },
+  [types.SET_PAGE]: (state, page) => {
+    state.page = page
   }
 }
 
@@ -40,6 +56,9 @@ const actions = {
   [types.SET_IMAGE_FROM_NAME]: ({ state, commit }, imageName) => {
     let image = state.images.find((x) => x.name === imageName)
     commit(types.SET_IMAGE_TO_SET, image)
+  },
+  [types.SET_PAGE]: ({ state, commit }, page) => {
+    commit(types.SET_PAGE, page)
   }
 }
 
